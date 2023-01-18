7b3ffe24c1a5bb017c682f763f8d85c6c135c756
Roman Blanco <rblanco@redhat.com>
2023-01-18 19:30:40 +0000

    cleanup: Drop support for OpenAPI (Swagger) v2

--- rswag-api/spec/rswag/api/middleware_spec.rb
+++ rswag-api/spec/rswag/api/middleware_spec.rb
@@ -6,9 +6,9 @@ module Rswag
 
     describe Middleware do
       let(:app) { double('app') }
-      let(:swagger_root) { File.expand_path('../fixtures/swagger', __FILE__) }
+      let(:openapi_root) { File.expand_path('../fixtures/openapi', __FILE__) }
       let(:config) do
-        Configuration.new.tap { |c| c.swagger_root = swagger_root }
+        Configuration.new.tap { |c| c.openapi_root = openapi_root }
       end
 
       subject { described_class.new(app, config) }
@@ -22,27 +22,27 @@ module Rswag
           }
         end
 
-        context 'given a path that maps to an existing swagger file' do
-          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/swagger.json') }
+        context 'given a path that maps to an existing openapi file' do
+          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/openapi.json') }
 
           it 'returns a 200 status' do
             expect(response.length).to eql(3)
             expect(response.first).to eql('200')
           end
 
-          it 'returns contents of the swagger file' do
+          it 'returns contents of the openapi file' do
             expect(response.length).to eql(3)
             expect(response[1]).to include( 'Content-Type' => 'application/json')
             expect(response[2].join).to include('"title":"API V1"')
           end
         end
 
-        context 'when swagger_headers is configured' do
-          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/swagger.json') }
+        context 'when openapi_headers is configured' do
+          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/openapi.json') }
 
           context 'replacing the default content type header' do
             before do
-              config.swagger_headers = { 'Content-Type' => 'application/json; charset=UTF-8' }
+              config.openapi_headers = { 'Content-Type' => 'application/json; charset=UTF-8' }
             end
             it 'returns a 200 status' do
               expect(response.length).to eql(3)
@@ -56,7 +56,7 @@ module Rswag
 
           context 'adding an additional header' do
             before do
-              config.swagger_headers = { 'Access-Control-Allow-Origin' => '*' }
+              config.openapi_headers = { 'Access-Control-Allow-Origin' => '*' }
             end
             it 'returns a 200 status' do
               expect(response.length).to eql(3)
@@ -73,7 +73,7 @@ module Rswag
           end
         end
 
-        context "given a path that doesn't map to any swagger file" do
+        context "given a path that doesn't map to any openapi file" do
           let(:env) { env_defaults.merge('PATH_INFO' => 'foobar.json') }
           before do
             allow(app).to receive(:call).and_return([ '500', {}, [] ])
@@ -95,28 +95,28 @@ module Rswag
           end
         end
 
-        context 'when the env contains a specific swagger_root' do
+        context 'when the env contains a specific openapi_root' do
           let(:env) do
             env_defaults.merge(
-              'PATH_INFO' => 'v1/swagger.json',
+              'PATH_INFO' => 'v1/openapi.json',
               'action_dispatch.request.path_parameters' => {
-                swagger_root: swagger_root
+                openapi_root: openapi_root
               }
             )
           end
 
-          it 'locates files at the provided swagger_root' do
+          it 'locates files at the provided openapi_root' do
             expect(response.length).to eql(3)
             expect(response[1]).to include( 'Content-Type' => 'application/json')
             expect(response[2].join).to include('"openapi":"3.0.1"')
           end
         end
 
-        context 'when a swagger_filter is configured' do
+        context 'when a openapi_filter is configured' do
           before do
-            config.swagger_filter = lambda { |swagger, env| swagger['host'] = env['HTTP_HOST'] }
+            config.openapi_filter = lambda { |openapi, env| openapi['host'] = env['HTTP_HOST'] }
           end
-          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/swagger.json') }
+          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/openapi.json') }
 
           it 'applies the filter prior to serialization' do
             expect(response.length).to eql(3)
@@ -124,15 +124,15 @@ module Rswag
           end
         end
 
-        context 'when a path maps to a yaml swagger file' do
-          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/swagger.yml') }
+        context 'when a path maps to a yaml openapi file' do
+          let(:env) { env_defaults.merge('PATH_INFO' => 'v1/openapi.yml') }
 
           it 'returns a 200 status' do
             expect(response.length).to eql(3)
             expect(response.first).to eql('200')
           end
 
-          it 'returns contents of the swagger file' do
+          it 'returns contents of the openapi file' do
             expect(response.length).to eql(3)
             expect(response[1]).to include( 'Content-Type' => 'text/yaml')
             expect(response[2].join).to include('title: API V1')
